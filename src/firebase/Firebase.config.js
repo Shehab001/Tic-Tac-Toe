a93acd79e993632cfe09ed4e586bdfa581941c40
Md. Shehab Chowdhury <shehab15-11396@diu.edu.bd>
2023-01-19 10:24:26 +0600

    authentication

---
+++ src/firebase/Firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxsoZDHtRwAi1KEYwSPpQwMaBZVA7MXiM",
+  authDomain: "brins-3be0f.firebaseapp.com",
+  projectId: "brins-3be0f",
+  storageBucket: "brins-3be0f.appspot.com",
+  messagingSenderId: "1024427150152",
+  appId: "1:1024427150152:web:8b52d234233eedf943c17d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
